037c0a8882aa500ad43aafe7256abb0ab138ca87
Andrei Karas <akaras@inbox.ru>
2014-11-12 18:22:33 +0300

    Into build scripts add support for build static 32 and 64 versions.

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+CMD="$1"
+
+if [[ -z "${CMD}" ]]; then
+    export CMD="default"
+fi
+
 autoreconf
-./configure --enable-sanitize --disable-lto --enable-packetver=20150000 --enable-debug=gdb
-make -j3
-make install
+if [[ "${CMD}" == "defaule" ]]; then
+    ./configure --enable-sanitize --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    make -j3
+    make install
+elif [[ "${CMD}" == "static" ]]; then
+    ./configure --disable-64bit --enable-static --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    make -j3
+elif [[ "${CMD}" == "static64" ]]; then
+    ./configure --enable-static --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    make -j3
+fi
