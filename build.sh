dd9a754cb8f026b8ed2fecd20e6ec2c679953294
Andrei Karas <akaras@inbox.ru>
2014-11-13 17:35:00 +0300

    fix static builds.

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ if [[ "${CMD}" == "defaule" ]]; then
     make -j3
     make install
 elif [[ "${CMD}" == "static" ]]; then
-    ./configure --disable-64bit --enable-static --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    ./configure LIBS="-lmysqlclient -lssl -lcrypto -pthread -lm -lz" --disable-64bit --enable-static --disable-lto --enable-packetver=20150000 --enable-debug=gdb
     make -j3
 elif [[ "${CMD}" == "static64" ]]; then
     ./configure --enable-static --disable-lto --enable-packetver=20150000 --enable-debug=gdb
