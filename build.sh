ecc0d45eab935d3800fd43dc1926b162a6b35978
Andrei Karas <akaras@inbox.ru>
2015-01-05 00:29:55 +0300

    In build script fix building with new mariadb.

--- build.sh
+++ build.sh
@@ -6,34 +6,36 @@ if [[ -z "${CMD}" ]]; then
     export CMD="default"
 fi
 
+export COMMON="--disable-lto --enable-packetver=20150000 --enable-debug=gdb --with-mysql=/usr/bin/mariadb_config"
+
 autoreconf
 if [[ "${CMD}" == "default" || "${CMD}" == "all" ]]; then
-    ./configure --enable-sanitize --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    ./configure --enable-sanitize ${COMMON}
     make -j3
     make install
     cd src/evol
     ./build.sh
 elif [[ "${CMD}" == "old" ]]; then
-    ./configure --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    ./configure ${COMMON}
     make -j3
     make install
     cd src/evol
     ./build.sh old
 elif [[ "${CMD}" == "valgrind" ]]; then
-    ./configure --disable-lto --enable-packetver=20150000 --enable-debug=gdb --enable-manager=no
+    ./configure --enable-manager=no ${COMMON}
     make -j3
     make install
     cd src/evol
     ./build.sh old
 elif [[ "${CMD}" == "server" ]]; then
-    ./configure --enable-sanitize --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    ./configure --enable-sanitize ${COMMON}
     make -j3
     make install
 elif [[ "${CMD}" == "static" ]]; then
-    ./configure LIBS="-lmysqlclient -lssl -lcrypto -pthread -lm -lz" --disable-64bit --enable-static --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    ./configure LIBS="-lmysqlclient -lssl -lcrypto -pthread -lm -lz" --disable-64bit --enable-static ${COMMON}
     make -j3
 elif [[ "${CMD}" == "static64" ]]; then
-    ./configure --enable-static --disable-lto --enable-packetver=20150000 --enable-debug=gdb
+    ./configure --enable-static ${COMMON}
     make -j3
 fi
 exit $?
