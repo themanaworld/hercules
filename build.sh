059b3c0dd1af3d03a8977ce46cab946481f7228a
Andrei Karas <akaras@inbox.ru>
2015-05-01 22:53:41 +0300

    use gcc 5 for build script.

--- build.sh
+++ build.sh
@@ -18,32 +18,33 @@ export COMMON="--disable-lto --enable-packetver=20150000 --enable-debug=gdb${SQL
 
 autoreconf
 if [[ "${CMD}" == "default" || "${CMD}" == "all" ]]; then
-    ./configure --enable-sanitize ${COMMON}
-    make -j3
+    export CC=gcc-5
+    ./configure --enable-manager=no --enable-sanitize=full ${COMMON}
+    make -j9
     make install
     cd src/evol
     ./build.sh
 elif [[ "${CMD}" == "old" ]]; then
     ./configure ${COMMON}
-    make -j3
+    make -j9
     make install
     cd src/evol
     ./build.sh old
 elif [[ "${CMD}" == "valgrind" ]]; then
     ./configure --enable-manager=no ${COMMON}
-    make -j3
+    make -j9
     make install
     cd src/evol
     ./build.sh old
 elif [[ "${CMD}" == "server" ]]; then
     ./configure --enable-sanitize ${COMMON}
-    make -j3
+    make -j9
     make install
 elif [[ "${CMD}" == "static" ]]; then
     ./configure LIBS="-lmysqlclient -lssl -lcrypto -pthread -lm -lz" --disable-64bit --enable-static ${COMMON}
-    make -j3
+    make -j9
 elif [[ "${CMD}" == "static64" ]]; then
     ./configure --enable-static ${COMMON}
-    make -j3
+    make -j9
 fi
 exit $?
