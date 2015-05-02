88ff6553ca19d83bac5bf34a13724763ce114937
Andrei Karas <akaras@inbox.ru>
2015-05-02 12:31:07 +0300

    In build script actual number of cores.

--- build.sh
+++ build.sh
@@ -16,35 +16,37 @@ fi
 
 export COMMON="--disable-lto --enable-packetver=20150000 --enable-debug=gdb${SQL}"
 
+export CORES=$(cat /proc/cpuinfo|grep processor|wc -l)
+
 autoreconf
 if [[ "${CMD}" == "default" || "${CMD}" == "all" ]]; then
     export CC=gcc-5
     ./configure --enable-manager=no --enable-sanitize=full ${COMMON}
-    make -j9
+    make -j${CORES}
     make install
     cd src/evol
     ./build.sh
 elif [[ "${CMD}" == "old" ]]; then
     ./configure ${COMMON}
-    make -j9
+    make -j${CORES}
     make install
     cd src/evol
     ./build.sh old
 elif [[ "${CMD}" == "valgrind" ]]; then
     ./configure --enable-manager=no ${COMMON}
-    make -j9
+    make -j${CORES}
     make install
     cd src/evol
     ./build.sh old
 elif [[ "${CMD}" == "server" ]]; then
     ./configure --enable-sanitize ${COMMON}
-    make -j9
+    make -j${CORES}
     make install
 elif [[ "${CMD}" == "static" ]]; then
     ./configure LIBS="-lmysqlclient -lssl -lcrypto -pthread -lm -lz" --disable-64bit --enable-static ${COMMON}
-    make -j9
+    make -j${CORES}
 elif [[ "${CMD}" == "static64" ]]; then
     ./configure --enable-static ${COMMON}
-    make -j9
+    make -j${CORES}
 fi
 exit $?
