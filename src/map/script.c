themanaworld/hercules

commit 5ceccb77064b557a2f616040f937417c8f002a72

add buildin add_group_command()

src/map/script.c
@@ -21499,6 +21499,43 @@ BUILDIN(issit)
 	return true;
 }
 
+BUILDIN(add_group_command)
+{
+	AtCommandInfo *acmd_d;
+	struct atcmd_binding_data *bcmd_d;
+	GroupSettings *group;
+	int group_index;
+	const char *atcmd = script_getstr(st, 2);
+	int group_id = script_getnum(st, 3);
+	bool self_perm = (script_getnum(st, 4) == 1);
+	bool char_perm = (script_getnum(st, 5) == 1);
+
+	if (!pcg->exists(group_id)) {
+		ShowWarning("script:add_group_command: group does not exist: %i\n", group_id);
+		script_pushint(st, 0);
+		return false;
+	}
+
+	group = pcg->id2group(group_id);
+	group_index = pcg->get_idx(group);
+
+	if ((bcmd_d = atcommand->get_bind_byname(atcmd)) != NULL) {
+		bcmd_d->at_groups[group_index] = self_perm;
+		bcmd_d->char_groups[group_index] = char_perm;
+		script_pushint(st, 1);
+		return true;
+	} else if ((acmd_d = atcommand->get_info_byname(atcmd)) != NULL) {
+		acmd_d->at_groups[group_index] = self_perm;
+		acmd_d->char_groups[group_index] = char_perm;
+		script_pushint(st, 1);
+		return true;
+	}
+
+	ShowWarning("script:add_group_command: command does not exist: %s\n", atcmd);
+	script_pushint(st, 0);
+	return false;
+}
+
 /**
  * @commands (script based)
  **/
@@ -21548,6 +21585,8 @@ BUILDIN(bindatcmd)
 		atcommand->binding[i]->group_lv = group_lv;
 		atcommand->binding[i]->group_lv_char = group_lv_char;
 		atcommand->binding[i]->log = log;
+		CREATE(atcommand->binding[i]->at_groups, char, db_size(pcg->db));
+		CREATE(atcommand->binding[i]->char_groups, char, db_size(pcg->db));
 	}
 
 	return true;
@@ -23719,6 +23758,7 @@ void script_parse_builtin(void) {
 		BUILDIN_DEF(useatcmd, "s"),
 		BUILDIN_DEF(has_permission, "v?"),
 		BUILDIN_DEF(can_use_command, "s?"),
+		BUILDIN_DEF(add_group_command, "siii"),
 
 		/**
 		 * Item bound [Xantara] [Akinari] [Mhalicot/Hercules]

commit a3db4dc0aed6d4d008eaa5c985df73143b2f12ad

fix memory leak introduced by 5ceccb7

src/map/script.c
@@ -5033,6 +5033,8 @@ void do_final_script(void)
 		aFree(script->str_buf);
 
 	for( i = 0; i < atcommand->binding_count; i++ ) {
+		aFree(atcommand->binding[i]->at_groups);
+		aFree(atcommand->binding[i]->char_groups);
 		aFree(atcommand->binding[i]);
 	}
 
@@ -5567,6 +5569,8 @@ int script_reload(void)
 	script->label_count = 0;
 
 	for( i = 0; i < atcommand->binding_count; i++ ) {
+		aFree(atcommand->binding[i]->at_groups);
+		aFree(atcommand->binding[i]->char_groups);
 		aFree(atcommand->binding[i]);
 	}
 